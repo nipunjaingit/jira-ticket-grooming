7f5b69216ccec540fbd9eb8cfc01f42463e7b04f
Nipun Jain <nipunjain171298@gmail.com>
2026-01-09 16:40:08 +0530

    Update BE service url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.SERVICE_URL || "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_SERVICE_URL || "http://localhost:3000";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
